73a97c2d66f7cd6c510dbb9b5f581b1c972850f5
Vini_Vergani <vinivergani95@gmail.com>
2022-04-26 18:16:42 -0300

    Feat: Adiciona requisito 5 parcial

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -8,21 +8,31 @@ function Login() {
         <label htmlFor="email">
           Email
           <input
+            name="email"
             data-testid="email-input"
             id="email"
+            type="email"
+            // value={ email }
+            // onChange={}
           />
         </label>
         <label htmlFor="password">
           Password
           <input
+            name="password"
             data-testid="password-input"
             id="password"
+            type="password"
+            // value={ password }
+            // onChange={}
           />
         </label>
 
         <button
           type="button"
           data-testid="login-submit-btn"
+          // disabled={ isDisabled }
+          // onClick={}
         >
           Enter
         </button>
