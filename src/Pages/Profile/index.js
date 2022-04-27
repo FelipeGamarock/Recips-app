e16c4819c192fd4f6487c8444bbaa63bc69a8879
Vini_Vergani <vinivergani95@gmail.com>
2022-04-27 17:56:14 -0300

    Fix: lint issues

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Footer from '../../Components/Footer';
 import { useHistory } from 'react-router-dom';
+import Footer from '../../Components/Footer';
 import HeaderForExplore from '../../Components/HeaderForExplore';
 
 function Profile() {
