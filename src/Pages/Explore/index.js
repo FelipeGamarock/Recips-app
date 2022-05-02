ef63df1843d8aef29195d5f0a16e1bff9d9bced4
Felipe Gama Danta <epilefgama2@gmail.com>
2022-05-02 15:58:24 -0300

    feat: Tela de explorar, requsitos 67 ao 69

--- src/Pages/Explore/index.js
+++ src/Pages/Explore/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../../Components/Footer';
 import HeaderForExplore from '../../Components/HeaderForExplore';
 
 function Explore() {
+  const history = useHistory();
+
   return (
     <div>
-      <h1>Explore</h1>
       <HeaderForExplore title="Explore" />
+      <div>
+        <button
+          type="button"
+          data-testid="explore-foods"
+          onClick={ () => history.push('/explore/foods') }
+        >
+          Explore Foods
+
+        </button>
+
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          onClick={ () => history.push('/explore/drinks') }
+        >
+          Explore Drinks
+
+        </button>
+      </div>
       <Footer />
     </div>
   );
